a565761b6e5be5d0dd6c492506289d8f174e2423
Milan Choudhary <choudharymilan08@gmail.com>
2026-01-24 21:59:45 +0530

    Insertion Sort updated

--- Sorting_Algorithms/Insertion_Sort.java
+++ Sorting_Algorithms/Insertion_Sort.java
@@ -15,6 +15,22 @@ public class Insertion_Sort {
             arr[i] = input.nextInt();
         }
 
+        for(int i = 0; i<n-1; i++){
+            int j = i+1;
+
+            while(j > 0 && arr[j] < arr[j-1]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j -= 1;
+            }
+
+        }
+
+        for(int i = 0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
         input.close();
 
     }
