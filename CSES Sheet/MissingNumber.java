6b8ab1857118dfa20e0e89a7f726891896879f5b
Milan Choudhary <choudharymilan08@gmail.com>
2026-04-09 14:18:00 +0530

    Minor changes in the code

--- CSES Sheet/MissingNumber.java
+++ CSES Sheet/MissingNumber.java
@@ -7,21 +7,32 @@ public class MissingNumber{
         Scanner input = new Scanner(System.in);
 
         int n = input.nextInt();
-        long val  = 0;
 
        int[] arr = new int[n-1];
+
+       int[] map = new int[n+1];
+       map[0] = 1;
        
         for(int i = 0; i<arr.length; i++){
             arr[i] = input.nextInt();
-            
+            map[arr[i]] += 1;
         }
 
-        int xor = 0;
+        int ans = 0;
 
-        for (int i = 1; i <= n; i++) 
-            xor ^= i;
+        for(int i = 1; i<=n; i++){
+            if(map[i] == 0){
+                ans = i;
+            }
+        }
 
-        System.out.println(xor);
+        System.out.println(ans);
+        // int xor = 0;
+
+        // for (int i = 1; i <= n; i++) 
+        //     xor ^= i;
+
+        // System.out.println(xor);
 
         input.close();
 
