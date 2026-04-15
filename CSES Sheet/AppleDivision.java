a76b8274492c53b1f5df63db41fadef482bd4fb4
Milan Choudhary <choudharymilan08@gmail.com>
2026-04-15 16:27:35 +0530

    Grid work

--- CSES Sheet/AppleDivision.java
+++ CSES Sheet/AppleDivision.java
@@ -8,12 +8,19 @@ public class AppleDivision {
         int n = input.nextInt();
 
         int[] arr = new int[n];
+        long sum = 0;
 
         for(int i = 0; i<n; i++){
             arr[i] = input.nextInt();
+            sum += arr[i];
         }
 
-        
+        if(sum%2 == 0){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(1);
+        }        
 
 
         input.close();
